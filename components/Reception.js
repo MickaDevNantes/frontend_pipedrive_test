2bce21dfe7cc9b2d577f13b19c0aef670904666f
mickael <robert.mickael28@gmail.com>
2024-07-25 14:42:08 +0200

    test webhook

--- components/Reception.js
+++ components/Reception.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 function Reception() {
+  const [webhook, setWebhook] = useState(null)
 
+  fetch('https://backend-pipedrive-test.vercel.app/getWebhook').then(reponse=>reponse.json()).then(data=>{
+    setWebhook(data.result)
+  })
   
+  let listeWebhooks = webhook.map((element, i)=><div key={i}><p>{element.action}</p><p>{element.object}</p><p>{element.company_id}</p><p>{element.user_id}</p></div>)
 
   return (
     <div>
@@ -10,6 +16,7 @@ function Reception() {
         <h1 className={styles.title}>
           Composant reception
         </h1>
+        {listeWebhooks}
       </main>
     </div>
   );
